00ef76fb6cd245b4a428cbd5f1d1002faea06e04
Graham Boyes <gr.ed.boyes@gmail.com>
2013-10-06 19:24:26 -0400

    -Basic functionality added for OMPursuitConstraint / OMPursuitCompoundConstraint, -Small changes to OMPursuitDictionary / OMPursuitSoundgrain

--- python/OMPursuitTest.py
+++ python/OMPursuitTest.py
@@ -3,5 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path = './test_sdifs/testSDIF5.sdif'
+constraint_path = './test_sdifs/constraint-SDIFs/test-constraints(single-conditional).pct.sdif'
 
 D = OMPursuit.OMPursuitDictionary(path, 1)
+C = OMPursuit.OMPursuitCompoundConstraint(constraint_path)
+
+print(C.cFullConstraintFunction(D, 45.0))
+
